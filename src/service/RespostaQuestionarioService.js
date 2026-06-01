74fd00366338df566428019612af8131640ad4ce
Lucca <lucca.f.livino@gmail.com>
2026-06-01 16:23:57 -0400

    refactor: Usando o método sanitize

--- src/service/RespostaQuestionarioService.js
+++ src/service/RespostaQuestionarioService.js
@@ -3,6 +3,7 @@ import QuestionarioRepository from '../repository/QuestionarioRepository.js';
 import PerguntaRepository from '../repository/PerguntaRepository.js';
 import RespostaQuestionarioRepository from '../repository/RespostaQuestionarioRepository.js';
 import AppError from '../utils/helpers/AppError.js';
+import { sanitizeDoc } from '../utils/helpers/sanitize.js';
 
 class RespostaQuestionarioService {
   constructor(
@@ -18,14 +19,7 @@ class RespostaQuestionarioService {
   }
 
   sanitize(doc) {
-    if (!doc) {
-      return null;
-    }
-
-    const raw = typeof doc?.toObject === 'function' ? doc.toObject() : doc;
-    const sanitized = { ...raw };
-    delete sanitized.__v;
-    return sanitized;
+    return sanitizeDoc(doc);
   }
 
   async resolverCandidatoId(candidatoId) {
